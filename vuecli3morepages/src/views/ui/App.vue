73c936042a9b543856e1ec3eb7aea48c7cd401ab
webtaotao <1138018907@qq.com>
2019-05-01 13:41:42 +0800

    vuecli3morepages

---
+++ vuecli3morepages/src/views/ui/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+      <router-link to="/">HomeUI</router-link>
+      <br>
+      <router-link to="/about">Aboutui</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+</script>
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
