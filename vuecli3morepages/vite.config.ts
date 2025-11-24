a25cec2a5c13e9f802edbb7d69212351b7fbc610
liutao <liutao@163.com>
2025-11-24 11:44:15 +0800

    11

--- vuecli3morepages/vite.config.ts
+++ vuecli3morepages/vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vuecli3pages/vuecli3morepages/',
+  base: '/vuecli3pages/vuecli3morepages/dist/',
   plugins: [vue()],
 })
